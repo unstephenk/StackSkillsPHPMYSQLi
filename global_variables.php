af15274d9e594edc4e7b43fe67ae2f5d5492fd57
unstephenk <unstephenk@gmail.com>
2016-01-15 12:49:11 -0600

    Second push from tutorials on PHP&MYSQL from StackSkills

---
+++ global_variables.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 1/15/2016
+ * Time: 12:43 PM
+ */
--- local_variables.php
+++ local_variables.php
@@ -6,6 +6,13 @@
  * Time: 11:02 PM
  */
 
+$name = "Stephen Kuehl";
+$age = 29;
 
+
+echo $name . " is " . $age ." Years Old";
+echo $city = " Dallas";
+echo $country = " TX";
+echo $phone = 2147931227;
 
 ?>

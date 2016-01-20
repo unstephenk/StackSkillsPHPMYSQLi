6d9a31f668b0df7e643f4c79bd3f4c536348144d
unstephenk <unstephenk@gmail.com>
2016-01-19 22:51:58 -0600

    PHP Error Handling

--- Tutorials/PHPMYSQL/error_handling.php
+++ Tutorials/PHPMYSQL/error_handling.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Error Handling</title>
+</head>
+<body>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +13,20 @@
  * Date: 1/19/2016
  * Time: 10:20 PM
  */
+
+/*$file = fopen("stephen.txt", "r") or die("Unable to read file.");*/
+
+if (!file_exists("create_a_test_file_data.txt")){
+    die("Unable to read the file.");
+}
+else{
+    $file = fopen("create_a_test_file_data.txt", "r");
+}
+
+
+
+?>
+
+
+</body>
+</html>

65fd470c0ce1b63766c65bff195fb117f6dd3f04
unstephenk <unstephenk@gmail.com>
2016-01-20 16:54:22 -0600

    MYSQLi Connection

---
+++ Tutorials/PHPMYSQL/mysqli_connection.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephenk
+ * Date: 1/20/2016
+ * Time: 4:32 PM
+ */
+
+$con = mysqli_connect("localhost","root","Nope!.","StackSkillsPHPMYSQLi") or die("Connection Failed");
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MYSQL Connection</title>
+</head>
+<body>
+
+</body>
+</html>

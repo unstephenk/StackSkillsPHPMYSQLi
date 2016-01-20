ab4e6489eee1fed3529a0902abded38d14defe39
unstephenk <unstephenk@gmail.com>
2016-01-19 22:18:52 -0600

    PHP Creating a text file

---
+++ creating_a_text_file.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Creating a Text File</title>
+</head>
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 1/18/2016
+ * Time: 10:27 PM
+ */
+
+/*$filename = "create_a_text_file_data.txt";
+$file = fopen($filename, "w") or die("Unable to open file!");
+
+$text = "Hello! I am adding text to this file";
+
+fwrite($file, $text);
+fclose($file);*/
+
+?>
+
+</body>
+</html>

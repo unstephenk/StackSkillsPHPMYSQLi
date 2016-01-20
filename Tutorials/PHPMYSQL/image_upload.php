9292147eaf5c66a1b3c3bef5cbbc3edff73f45c6
unstephenk <unstephenk@gmail.com>
2016-01-19 23:06:03 -0600

    PHP Image Upload

---
+++ Tutorials/PHPMYSQL/image_upload.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Image Upload</title>
+</head>
+<body>
+
+<!--<form method="post" action="image_upload.php" enctype="multipart/form-data">
+    <input type="file" name="image"/>
+    <input type="submit" name="upoad" value="Upload Image"/>
+</form>-->
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 1/19/2016
+ * Time: 10:52 PM
+ */
+
+echo "The lines in this code have been commented out for safety reasons."
+
+/*if(isset($_POST["upload"])){
+    $image = $_FILES['image']['name'];
+    $image_tmp = $_FILES['image']['tmp_image'];
+
+    move_uploaded_file($image_tmp,"Uploaded_Images/$image");
+
+    echo "<img src='Uploaded_Images/$image' width='500' height='400'/>";
+}*/
+
+
+?>
+
+
+</body>
+</html>

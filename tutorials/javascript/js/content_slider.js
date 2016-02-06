6d3757c9a8fb3983a996e01fffa83cfe95cc17c8
unstephenk <unstephenk@gmail.com>
2016-02-06 11:31:43 -0600

    4th Javascript tutorial - jQuery Fundamentals

---
+++ tutorials/javascript/js/content_slider.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Stephen on 2/6/2016.
+ */
+
+$(document).ready(function(){
+
+   /* $('.myLink').on('mouseenter', function(){
+        $('h1').addClass('red');
+    });
+
+    $('.myLink').on('mouseleave', function(){
+        $('h1').removeClass('red');
+    });*/
+
+    /*$('.myLink').click(function(){
+        $('h1').replaceWith('<h3>Hello World</h3>');
+;    })*/
+
+    $('.myLink').click(function(){
+        $('h1').css('font-size','40px');
+    });
+
+});

a17dd7011f911240c5f8c579bd518a56a8da600b
unstephenk <unstephenk@gmail.com>
2016-02-08 17:19:01 -0600

    4th Angular Tutorial - Form Validation using HTML5

---
+++ tutorials/angularjs/js/lesson4.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Stephen on 2/8/2016.
+ */
+
+var app = angular.module('minmax', []);
+
+app.controller('MinMaxCtrl', function($scope, $http){
+    $scope.formModel={};
+
+    $scope.onSubmit = function (){
+        console.log("Hey! I'm submitted.");
+        console.log($scope.formModel);
+
+        $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+        success(function (data){
+            console.log(":)");
+        }).error(function(data){
+            console.log(':(');
+        });
+    };
+});
